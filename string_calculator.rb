603772e58752ab54c553d5a07f49515b4d620a06
anupamroy8 <anupamroy8@gmail.com>
2025-01-12 19:40:06 +0530

    add test to handle newlines as delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,6 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.match?(/^\d+$/)
-    numbers.split(",").map(&:to_i).sum
+    numbers.gsub("\n", ",").split(",").map(&:to_i).sum
   end
 end
